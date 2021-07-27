e75d50d23a97eb5e706f4f377d3491a9d38d3597
Yvonne Wood <yvonatron@gmail.com>
2021-07-27 20:04:22 +0100

    Add project files.

---
+++ Controllers/BillingSwaggerClient.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace ZetiTest.Controllers {
+
+    [System.CodeDom.Compiler.GeneratedCode("NSwag", "13.0.5.0 (NJsonSchema v10.0.22.0 (Newtonsoft.Json v11.0.0.0))")]
+    public partial class BillingSwaggerClient {
+        private string _baseUrl = "https://funczetiinterviewtest.azurewebsites.net";
+        private System.Net.Http.HttpClient _httpClient;
+        private System.Lazy<Newtonsoft.Json.JsonSerializerSettings> _settings;
+
+        public BillingSwaggerClient(System.Net.Http.HttpClient httpClient) {
+            _httpClient = httpClient;
+            _settings = new System.Lazy<Newtonsoft.Json.JsonSerializerSettings>(() =>
+            {
+                var settings = new Newtonsoft.Json.JsonSerializerSettings();
+                UpdateJsonSerializerSettings(settings);
+                return settings;
+            });
+        }
+
+        public string BaseUrl
+        {
+            get { return _baseUrl; }
+            set { _baseUrl = value; }
+        }
+
+        protected Newtonsoft.Json.JsonSerializerSettings JsonSerializerSettings { get { return _settings.Value; } }
+
+        partial void UpdateJsonSerializerSettings(Newtonsoft.Json.JsonSerializerSettings settings);
+        partial void PrepareRequest(System.Net.Http.HttpClient client, System.Net.Http.HttpRequestMessage request, string url);
+        partial void PrepareRequest(System.Net.Http.HttpClient client, System.Net.Http.HttpRequestMessage request, System.Text.StringBuilder urlBuilder);
+        partial void ProcessResponse(System.Net.Http.HttpClient client, System.Net.Http.HttpResponseMessage response);
+
+        /// <returns>Success</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        public System.Threading.Tasks.Task VehiclesAsync() {
+            return VehiclesAsync(System.Threading.CancellationToken.None);
+        }
+
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <returns>Success</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        public async System.Threading.Tasks.Task<System.Collections.Generic.ICollection<Vehicle>> VehiclesAsync(System.Threading.CancellationToken cancellationToken) {
+            var urlBuilder_ = new System.Text.StringBuilder();
+            urlBuilder_.Append(BaseUrl != null ? BaseUrl.TrimEnd('/') : "").Append("/api/vehicles");
+
+            var client_ = _httpClient;
+            try
+            {
+                using (var request_ = new System.Net.Http.HttpRequestMessage())
+                {
+                    request_.Method = new System.Net.Http.HttpMethod("GET");
+
+                    PrepareRequest(client_, request_, urlBuilder_);
+                    var url_ = urlBuilder_.ToString();
+                    request_.RequestUri = new System.Uri(url_, System.UriKind.RelativeOrAbsolute);
+                    PrepareRequest(client_, request_, url_);
+
+                    var response_ = await client_.SendAsync(request_, System.Net.Http.HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
+                    try
+                    {
+                        var headers_ = System.Linq.Enumerable.ToDictionary(response_.Headers, h_ => h_.Key, h_ => h_.Value);
+                        if (response_.Content != null && response_.Content.Headers != null)
+                        {
+                            foreach (var item_ in response_.Content.Headers)
+                                headers_[item_.Key] = item_.Value;
+                        }
+
+                        ProcessResponse(client_, response_);
+
+                        var status_ = ((int)response_.StatusCode).ToString();
+                        if (status_ == "200")
+                        {
+                            var objectResponse_ = await ReadObjectResponseAsync<System.Collections.Generic.ICollection<Vehicle>>(response_, headers_).ConfigureAwait(false);
+                            return objectResponse_.Object;
+                        }
+                        else
+                        if (status_ != "200" && status_ != "204")
+                        {
+                            var responseData_ = response_.Content == null ? null : await response_.Content.ReadAsStringAsync().ConfigureAwait(false);
+                            throw new ApiException("The HTTP status code of the response was not expected (" + (int)response_.StatusCode + ").", (int)response_.StatusCode, responseData_, headers_, null);
+                        }
+                        return default(System.Collections.Generic.ICollection<Vehicle>);
+                    }
+                    finally
+                    {
+                        if (response_ != null)
+                            response_.Dispose();
+                    }
+                }
+            }
+            finally
+            {
+            }
+        }
+
+        /// <returns>Success</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        public System.Threading.Tasks.Task<System.Collections.Generic.ICollection<VehicleHistory>> HistoryAsync(string asAtDateTimeString) {
+            return HistoryAsync(asAtDateTimeString, System.Threading.CancellationToken.None);
+        }
+
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <returns>Success</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        public async System.Threading.Tasks.Task<System.Collections.Generic.ICollection<VehicleHistory>> HistoryAsync(string asAtDateTimeString, System.Threading.CancellationToken cancellationToken) {
+            if (asAtDateTimeString == null)
+                throw new System.ArgumentNullException("asAtDateTimeString");
+
+            var urlBuilder_ = new System.Text.StringBuilder();
+            urlBuilder_.Append(BaseUrl != null ? BaseUrl.TrimEnd('/') : "").Append("/api/vehicles/history/{asAtDateTimeString}");
+            urlBuilder_.Replace("{asAtDateTimeString}", System.Uri.EscapeDataString(ConvertToString(asAtDateTimeString, System.Globalization.CultureInfo.InvariantCulture)));
+
+            var client_ = _httpClient;
+            try
+            {
+                using (var request_ = new System.Net.Http.HttpRequestMessage())
+                {
+                    request_.Method = new System.Net.Http.HttpMethod("GET");
+
+                    PrepareRequest(client_, request_, urlBuilder_);
+                    var url_ = urlBuilder_.ToString();
+                    request_.RequestUri = new System.Uri(url_, System.UriKind.RelativeOrAbsolute);
+                    PrepareRequest(client_, request_, url_);
+
+                    var response_ = await client_.SendAsync(request_, System.Net.Http.HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
+                    try
+                    {
+                        var headers_ = System.Linq.Enumerable.ToDictionary(response_.Headers, h_ => h_.Key, h_ => h_.Value);
+                        if (response_.Content != null && response_.Content.Headers != null)
+                        {
+                            foreach (var item_ in response_.Content.Headers)
+                                headers_[item_.Key] = item_.Value;
+                        }
+
+                        ProcessResponse(client_, response_);
+
+                        var status_ = ((int)response_.StatusCode).ToString();
+                        if (status_ == "200")
+                        {
+                            var objectResponse_ = await ReadObjectResponseAsync<System.Collections.Generic.ICollection<VehicleHistory>>(response_, headers_).ConfigureAwait(false);
+                            return objectResponse_.Object;
+                        }
+                        else
+                        if (status_ != "200" && status_ != "204")
+                        {
+                            var responseData_ = response_.Content == null ? null : await response_.Content.ReadAsStringAsync().ConfigureAwait(false);
+                            throw new ApiException("The HTTP status code of the response was not expected (" + (int)response_.StatusCode + ").", (int)response_.StatusCode, responseData_, headers_, null);
+                        }
+                        return default(System.Collections.Generic.ICollection<VehicleHistory>);
+                    }
+                    finally
+                    {
+                        if (response_ != null)
+                            response_.Dispose();
+                    }
+                }
+            }
+            finally
+            {
+            }
+        }
+
+        protected struct ObjectResponseResult<T> {
+            public ObjectResponseResult(T responseObject, string responseText) {
+                this.Object = responseObject;
+                this.Text = responseText;
+            }
+
+            public T Object { get; }
+
+            public string Text { get; }
+        }
+
+        public bool ReadResponseAsString { get; set; }
+
+        protected virtual async System.Threading.Tasks.Task<ObjectResponseResult<T>> ReadObjectResponseAsync<T>(System.Net.Http.HttpResponseMessage response, System.Collections.Generic.IReadOnlyDictionary<string, System.Collections.Generic.IEnumerable<string>> headers) {
+            if (response == null || response.Content == null)
+            {
+                return new ObjectResponseResult<T>(default(T), string.Empty);
+            }
+
+            if (ReadResponseAsString)
+            {
+                var responseText = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                try
+                {
+                    var typedBody = Newtonsoft.Json.JsonConvert.DeserializeObject<T>(responseText, JsonSerializerSettings);
+                    return new ObjectResponseResult<T>(typedBody, responseText);
+                }
+                catch (Newtonsoft.Json.JsonException exception)
+                {
+                    var message = "Could not deserialize the response body string as " + typeof(T).FullName + ".";
+                    throw new ApiException(message, (int)response.StatusCode, responseText, headers, exception);
+                }
+            }
+            else
+            {
+                try
+                {
+                    using (var responseStream = await response.Content.ReadAsStreamAsync().ConfigureAwait(false))
+                    using (var streamReader = new System.IO.StreamReader(responseStream))
+                    using (var jsonTextReader = new Newtonsoft.Json.JsonTextReader(streamReader))
+                    {
+                        var serializer = Newtonsoft.Json.JsonSerializer.Create(JsonSerializerSettings);
+                        var typedBody = serializer.Deserialize<T>(jsonTextReader);
+                        return new ObjectResponseResult<T>(typedBody, string.Empty);
+                    }
+                }
+                catch (Newtonsoft.Json.JsonException exception)
+                {
+                    var message = "Could not deserialize the response body stream as " + typeof(T).FullName + ".";
+                    throw new ApiException(message, (int)response.StatusCode, string.Empty, headers, exception);
+                }
+            }
+        }
+        private string ConvertToString(object value, System.Globalization.CultureInfo cultureInfo) {
+            if (value is System.Enum)
+            {
+                string name = System.Enum.GetName(value.GetType(), value);
+                if (name != null)
+                {
+                    var field = System.Reflection.IntrospectionExtensions.GetTypeInfo(value.GetType()).GetDeclaredField(name);
+                    if (field != null)
+                    {
+                        var attribute = System.Reflection.CustomAttributeExtensions.GetCustomAttribute(field, typeof(System.Runtime.Serialization.EnumMemberAttribute))
+                            as System.Runtime.Serialization.EnumMemberAttribute;
+                        if (attribute != null)
+                        {
+                            return attribute.Value != null ? attribute.Value : name;
+                        }
+                    }
+                }
+            }
+            else if (value is bool)
+            {
+                return System.Convert.ToString(value, cultureInfo).ToLowerInvariant();
+            }
+            else if (value is byte[])
+            {
+                return System.Convert.ToBase64String((byte[])value);
+            }
+            else if (value != null && value.GetType().IsArray)
+            {
+                var array = System.Linq.Enumerable.OfType<object>((System.Array)value);
+                return string.Join(",", System.Linq.Enumerable.Select(array, o => ConvertToString(o, cultureInfo)));
+            }
+
+            return System.Convert.ToString(value, cultureInfo);
+        }
+    }
+
+    [System.CodeDom.Compiler.GeneratedCode("NSwag", "13.0.5.0 (NJsonSchema v10.0.22.0 (Newtonsoft.Json v11.0.0.0))")]
+    public partial class ApiException : System.Exception {
+        public int StatusCode { get; private set; }
+
+        public string Response { get; private set; }
+
+        public System.Collections.Generic.IReadOnlyDictionary<string, System.Collections.Generic.IEnumerable<string>> Headers { get; private set; }
+
+        public ApiException(string message, int statusCode, string response, System.Collections.Generic.IReadOnlyDictionary<string, System.Collections.Generic.IEnumerable<string>> headers, System.Exception innerException)
+            : base(message + "\n\nStatus: " + statusCode + "\nResponse: \n" + response.Substring(0, response.Length >= 512 ? 512 : response.Length), innerException) {
+            StatusCode = statusCode;
+            Response = response;
+            Headers = headers;
+        }
+
+        public override string ToString() {
+            return string.Format("HTTP Response: \n\n{0}\n\n{1}", Response, base.ToString());
+        }
+    }
+
+    [System.CodeDom.Compiler.GeneratedCode("NSwag", "13.0.5.0 (NJsonSchema v10.0.22.0 (Newtonsoft.Json v11.0.0.0))")]
+    public partial class ApiException<TResult> : ApiException {
+        public TResult Result { get; private set; }
+
+        public ApiException(string message, int statusCode, string response, System.Collections.Generic.IReadOnlyDictionary<string, System.Collections.Generic.IEnumerable<string>> headers, TResult result, System.Exception innerException)
+            : base(message, statusCode, response, headers, innerException) {
+            Result = result;
+        }
+    }
+
+    [System.CodeDom.Compiler.GeneratedCode("NJsonSchema", "10.0.22.0 (Newtonsoft.Json v11.0.0.0)")]
+    public partial class Vehicle {
+        [JsonPropertyName("vin")]
+        public string Vin { get; set; }
+        [JsonPropertyName("licensePlate")]
+        public string LicensePlate { get; set; }
+        [JsonPropertyName("make")]
+        public string Make { get; set; }
+        [JsonPropertyName("model")]
+        public string Model { get; set; }
+        [JsonPropertyName("state")]
+        public VehicleHistory State { get; set; }
+
+        public Vehicle Create() {
+            this.Make = "Mercedes";
+            this.Model = "SLK360";
+            this.LicensePlate = "181 WZZ";
+            this.Vin = "1231245245HGy";
+            return this;
+        }
+    }
+
+
+    [System.CodeDom.Compiler.GeneratedCode("NJsonSchema", "10.0.22.0 (Newtonsoft.Json v11.0.0.0)")]
+    public class VehicleHistory : Vehicle {
+        [JsonPropertyName("odometerInMeters")]
+        public float OdometerInMeters { get; set; }
+        [JsonPropertyName("speedInMph")]
+        public float SpeedInMph { get; set; }
+        [JsonPropertyName("asAt")]
+        public DateTime AsAt { get; set; }
+    }
+
+    [System.CodeDom.Compiler.GeneratedCode("NJsonSchema", "10.0.22.0 (Newtonsoft.Json v11.0.0.0)")]
+    public class ReadingEntry : Vehicle {
+        [JsonPropertyName("vin")]
+        public string Vin { get; set; }
+        [JsonPropertyName("odometerInMeters")]
+        public float OdometerInMeters { get; set; }
+
+        public ReadingEntry(string plate, float mileageInMeters) {
+            this.LicensePlate = plate;
+            this.OdometerInMeters = mileageInMeters;
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,132 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using ZetiTest.Controllers;
+
+namespace ZetiTest.VehicleBillingClient {
+    class Program {
+        //Client Code Fields for async calls to OpenAPI
+        private static HttpClient _client = HttpClientFactory.Create();       
+        public static CancellationToken _cancellationToken = new CancellationToken();
+        private static string _startDate = "2021-02-01T00:00:00+00:00";
+        private static string _endDate = "2021-02-28T23:59:00Z";
+
+        //Customer specific Billing Fields
+        private static string _assetOperator = "Bob's Taxis";
+        private static double _billTotal = 0.00;
+        // Feed-data is in Meters. Conversion rate  => Miles
+        private static double _metersToMiles = 1609.34;
+        // In £ per mile
+        private static double _ratePerMile = 0.207;
+
+        static void Main(string[] args) {
+            GetVehicleBillingAsync();
+        }
+
+        static async void GetVehicleBillingAsync() {
+            /* 
+             * I modified generated OpenAPIClient to return ICollection<T> instead of Task Void
+             * Also I included the types I needed inside 
+             * VehicleTest\Controllers\BillingSwaggerClient.cs
+             */
+            BillingSwaggerClient billingClient = new BillingSwaggerClient(_client);
+            var requestVehiclesAll = billingClient.VehiclesAsync(_cancellationToken).Result;
+            var requestHistoryStart = billingClient.HistoryAsync(_startDate, _cancellationToken).Result;
+            var requestHistoryEnd = billingClient.HistoryAsync(_endDate, _cancellationToken).Result;
+
+            await Task.FromResult(requestVehiclesAll);
+            await Task.FromResult(requestHistoryStart);
+            await Task.FromResult(requestHistoryEnd);
+
+            var mergedList = requestHistoryStart.Union(requestHistoryEnd).ToList();
+
+            GenerateBill(billingClient, requestVehiclesAll, requestHistoryStart, requestHistoryEnd);
+        }
+
+        static void GenerateBill(BillingSwaggerClient billingClient, ICollection<Vehicle> fleet, ICollection<VehicleHistory> historyStart, ICollection<VehicleHistory> historyEnd) {
+
+            try
+            {
+                // This step for clarity and readability
+                ICollection<Vehicle> fleetAllList = fleet;
+                ICollection<VehicleHistory> historyStartList = historyStart;
+                ICollection<VehicleHistory> historyEndList = historyEnd;
+
+                //Setup Initial runtime vars
+                var mileageStart = 000000.0;
+                var mileageEnd = 099999.0;
+                var vin = "";
+
+                //Bring List startDate and EndDate into one list for iteration
+                var mergedList = historyStartList.Union(historyEndList).ToList();
+                //Used for matching up Lisence plates for mileage calcs and Invoice creation
+                var tempList = new List<VehicleHistory>();
+
+                foreach (var vehicle in fleet)
+                {
+                    vin = vehicle.LicensePlate;
+                    foreach (var record in mergedList)
+                    {
+                        if (record.LicensePlate.Equals(vin))
+                        {
+                            tempList.Add(record);
+                        };
+                    }
+
+                    mileageStart = tempList[0].State.OdometerInMeters;
+                    mileageEnd = tempList[1].State.OdometerInMeters;
+                    CalculateCost(vin, mileageStart, mileageEnd);
+                    tempList.Clear();
+                    Console.WriteLine("Asset:" + vin);
+                }
+                GenerateInvoice(_billTotal);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }           
+        }
+
+        /* This section builds up a JSON object with relevant detail for Invoice
+         * Called in GenerateInvoice() 
+        */
+        static JObject itemizedBill = new();
+        public static void CalculateCost(string id, double start, double stop) {
+            var chargePerVehicle = (stop - start) / _metersToMiles * _ratePerMile;
+            itemizedBill.Add("Asset:" + id, "OStart/metres:" + start);
+            itemizedBill.Add("Asset1:" + id, "OStop/metres:" + stop);
+            itemizedBill.Add("DistanceMetres:" + id, (stop - start));
+            itemizedBill.Add("DistanceMiles:" + id, (stop - start) / _metersToMiles);
+            itemizedBill.Add("ChargePerVehicle:" + id, Math.Round(chargePerVehicle, 2));
+            _billTotal += Math.Round(chargePerVehicle, 2);
+        }
+
+        //Generate JSON Bill statement    
+        public static JObject GenerateInvoice(double total) {
+            //To £ from p
+
+            JObject jBill = JObject.FromObject(new
+            {
+                bill = new
+                {
+                    title = "Invoice " + _assetOperator.ToString(),
+                    subtitle = "Generated on  " + DateTime.Now.ToLongDateString(),
+                    link = "http://www.zetiorg.com",
+                    detail = itemizedBill,
+                    description = "Rate per Mile in £: " + _ratePerMile,
+                    summary = "Total Due in £: " + Math.Round(total, 2)
+                }
+            });
+            Console.WriteLine("Json Bill generated.");
+            Console.WriteLine("See preview below:");
+            Console.WriteLine("___-----------___");
+            Console.Write(jBill);
+            return jBill;
+        }
+    }
+}
